1887eec0fc3050957d82be846c84b7285beb7a70
kauazin20 <kauaalmeidam5@gmail.com>
2025-06-17 21:35:06 +0000

    Movimentando as Peças do xadrez, feito!

--- xadrez.c
+++ xadrez.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    // Quantidade de casas a se mover
+
     const int casasTorre = 5;
     const int casasBispo = 5;
     const int casasRainha = 8;
 
     printf("Movimento da Torre (for) - 5 casas para a direita:\n");
-    // Torre se move em linha reta, horizontal ou vertical - vamos para a direita
+    
     for (int i = 1; i <= casasTorre; i++) {
         printf("Direita\n");
     }
 
     printf("\nMovimento do Bispo (while) - 5 casas na diagonal para cima e direita:\n");
-    // Bispo se move diagonal - combina "Cima, Direita"
+   
     int countBispo = 1;
     while (countBispo <= casasBispo) {
         printf("Cima, Direita\n");
@@ -21,7 +21,7 @@ int main() {
     }
 
     printf("\nMovimento da Rainha (do-while) - 8 casas para a esquerda:\n");
-    // Rainha pode mover em todas as direções - aqui vamos para a esquerda
+
     int countRainha = 1;
     do {
         printf("Esquerda\n");
